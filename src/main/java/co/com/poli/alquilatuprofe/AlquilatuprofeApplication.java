3ff615da235df47673d7dad47cefea84b48d118f
Wilmer Moreno <morenowilmer115@gmail.com>
2024-11-17 22:27:20 -0500

    Inicio

---
+++ src/main/java/co/com/poli/alquilatuprofe/AlquilatuprofeApplication.java
@@ -0,0 +1,13 @@
+package co.com.poli.alquilatuprofe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilatuprofeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilatuprofeApplication.class, args);
+	}
+
+}
