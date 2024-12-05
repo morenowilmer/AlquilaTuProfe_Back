5fd87746387032d99bbde6ff8d4f4d102439f9de
Wilmer Moreno <morenowilmer115@gmail.com>
2024-12-04 23:01:57 -0500

    Implementación registro de categoría

--- src/main/java/co/com/poli/alquilatuprofe/model/exception/SinAutorizacionException.java
+++
@@ -1,8 +0,0 @@
-package co.com.poli.alquilatuprofe.model.exception;
-
-public class SinAutorizacionException extends Exception {
-
-    public SinAutorizacionException(String message) {
-        super(message);
-    }
-}
